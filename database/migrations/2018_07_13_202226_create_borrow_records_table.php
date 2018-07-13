8109073d04eaf62cf214d5104b848e7a17f15421
Kirk Charles Niverba <kirkniverba23@gmail.com>
2018-07-13 20:26:25 +0800

    Recreated the Borrow Records Table Entry

--- database/migrations/2018_07_13_202226_create_borrow_records_table.php
+++ database/migrations/2018_07_13_202226_create_borrow_records_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBorrowRecords extends Migration
+class CreateBorrowRecordsTable extends Migration
 {
     /**
      * Run the migrations.
