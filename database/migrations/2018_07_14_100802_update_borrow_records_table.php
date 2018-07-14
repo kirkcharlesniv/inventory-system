e9412ec8fff6e5115e9a5b840234c470880c20f9
Kirk Charles Niverba <kirkniverba23@gmail.com>
2018-07-14 10:22:49 +0800

    Fix update borrow records table

--- database/migrations/2018_07_14_100802_update_borrow_records_table.php
+++ database/migrations/2018_07_14_100802_update_borrow_records_table.php
@@ -16,10 +16,8 @@ class UpdateBorrowRecordsTable extends Migration
         Schema::table('borrow_records', function (Blueprint $table) {
             $table->bigInteger('returned')->default('0')->change();
             $table->bigInteger('status')->default('0')->change();
-            $table->timestamp('created_at')->nullable()->change();
-            $table->timestamp('updated_at')->nullable()->change();
-            $table->timestamp('created_at')->default(null)->change();
-            $table->timestamp('updated_at')->default(null)->change();
+            $table->dropTimestamps();
+            $table->nullableTimestamps();
         });
     }
 
