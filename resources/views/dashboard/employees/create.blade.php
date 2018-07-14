e0fd3685f8d94df29fe1f7f47a3ec044ed7d22a5
Kirk Charles Niverba <kirkniverba23@gmail.com>
2018-07-14 09:36:14 +0800

    Readded the mask function

--- resources/views/dashboard/employees/create.blade.php
+++ resources/views/dashboard/employees/create.blade.php
@@ -19,7 +19,7 @@
     </div>
     <div class="form-group">
         {{Form::label('phone', 'Phone Number')}}
-        {{Form::text('phone', '09', ['class' => 'form-control phonenumber', 'required' => 'required', 'minlength' => '13', 'maxlength' => '13'])}}
+        {{Form::text('phone', '', ['class' => 'form-control phonenumber', 'required' => 'required', 'minlength' => '13', 'maxlength' => '13'])}}
     </div>
     <div class="form-group">
         {{Form::label('tin_number', 'TIN Number')}}
@@ -34,7 +34,7 @@
     <script>
         $(document).ready(function($){
             $('.tin_mask').mask("999-999-999");
-            $('.phonenumber').mask("+639000000000");
+            $('.phonenumber').mask("09#########");
         });
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
