1131d93c1009a858accf4c682c80332050cc837a
Kirk Charles Niverba <kirkniverba23@gmail.com>
2018-07-14 09:24:18 +0800

    added a repo of jquery mask

--- resources/views/dashboard/employees/create.blade.php
+++ resources/views/dashboard/employees/create.blade.php
@@ -33,8 +33,7 @@
     {!! Form::close() !!}
     <script>
         $(document).ready(function($){
-            $('.tin_mask').mask("999-999-999-000");
+            $('.tin_mask').mask("999-999-999");
         });
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
 @endsection
