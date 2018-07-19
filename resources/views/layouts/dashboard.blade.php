e20ef07de633317b46f93c46d2c445f7c7837547
Kirk Charles Niverba <kirkniverba23@gmail.com>
2018-07-19 16:05:25 +0800

    ok

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -151,7 +151,7 @@
                             &copy;
                             <script> document.write(new Date().getFullYear().toString()) </script>,
                             Designed and Coded by
-                                <a href="https://facebook.com/callmackoy" target="_blank">Mark Elias Gunao and </a> <a href="https://facebook.com/kirk.niverba.9" target="_blank"> Kirk Charles Niverba</a>. Template by
+                                <a href="https://facebook.com/callmackoy" target="_blank">Mark Elias Gunao</a> and <a href="https://facebook.com/kirk.niverba.9" target="_blank"> Kirk Charles Niverba</a>. Template by
                                 <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
                         </div>
                     </div>
