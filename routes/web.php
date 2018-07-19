287c5aed3ecef461e2a4a4657325b9f22f93a272
Kirk Charles Niverba <kirkniverba23@gmail.com>
2018-07-19 13:07:05 +0800

    change orders

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,11 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('home/borrow/search', 'BorrowsController@search')->name('search');
     Route::get('home/borrow/employeerecords', 'BorrowsController@employeerecords')->name('search');
     Route::get('home/borrow/employeenames', 'BorrowsController@employeenames')->name('name_search');
+    Route::get('home/items/decrement', 'ItemsController@decrement')->name('decrement');
 
     Route::resource('home/employees', 'EmployeesController');
     Route::resource('home/borrow', 'BorrowsController');
     Route::resource('home/items', 'ItemsController');
-
-    Route::get('home/items/decrement', 'ItemsController@decrement')->name('decrement');
 
     Route::get('home/export', 'DashboardController@download');
 });
